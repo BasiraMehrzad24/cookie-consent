54136c5fef62806ccaebac44523a958685eeccb4
BasiraMehrzad24 <basiramehrzad25@gmail.com>
2024-02-18 21:47:24 +0430

    cookies are done

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById("modal-text")
+const declineBtn = document.getElementById("decline-btn")
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
+
 
 setTimeout(function () {
     modal.style.display = 'inline'
@@ -11,8 +14,18 @@ modalCloseBtn.addEventListener('click', function () {
     modal.style.display = 'none'
 })
 
+declineBtn.addEventListener('mouseenter', function () {
+    modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+
+})
+
+
 consentForm.addEventListener('submit', function (e) {
     e.preventDefault()
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+
+
     modalText.innerHTML = `
 <div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
@@ -27,12 +40,13 @@ consentForm.addEventListener('submit', function (e) {
 
     setTimeout(function () {
         document.getElementById('modal-inner').innerHTML = `
-    <h2>Thanks you sucker! </h2>
+    <h2>Thanks <span class="modal-display-name">${fullName} </span>  you sucker! </h2>
     <p>We just sold the rights to your eternal soul.</p>
     <div class="idiot-gif">
         <img src="images/giphy.gif">
     </div>
 `
+        modalCloseBtn.disabled = false
     }, 3000)
 
 
